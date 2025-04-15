fbea1bf68ecabfdc866a34c926f0668e476f1355
davila <davilaketilly59@gmail.com>
2025-04-15 11:43:51 -0300

    atividades

---
+++ exercicio08.py
@@ -0,0 +1,9 @@
+n= int(input("digite o numero da media"))
+for i in range (1,n+1):
+    notas= int (input("digite o numero"))
+    somanotas = notas + notas
+    media=somanotas/n
+print (f" sua media foi {media}")
+
+
+
